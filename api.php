5025a3b5dd1ee863a7a54569eb594a0718ac9fd6
mkbodanu4 <mailmanbo@gmail.com>
2022-07-26 22:42:45 +0300

    Added filter "since"

--- api.php
+++ api.php
@@ -10,6 +10,13 @@ if (!isset($_GET['key']) || $_GET['key'] !== getenv('API_KEY')) {
 $get = trim(filter_var($_GET['get'], FILTER_SANITIZE_FULL_SPECIAL_CHARS));
 switch ($get) {
     case 'history':
+        $since_date = NULL;
+        $since = trim(filter_var($_GET['since'], FILTER_SANITIZE_FULL_SPECIAL_CHARS));
+        if ($since) {
+            $since_timestamp = strtotime($since);
+            if ($since_timestamp && $since_timestamp >= strtotime("1 year ago"))
+                $since_date = date("Y-m-d H:i:s", $since_timestamp);
+        }
         $balloons = array();
 
         $query = "SELECT `call_sign` FROM `history` GROUP BY `call_sign`;";
@@ -22,9 +29,12 @@ switch ($get) {
                 'history' => array()
             );
 
-            $history_query = "SELECT `date`, `latitude`, `longitude`, `course`, `speed`, `altitude`, `daodatumbyte`, `comment` FROM `history` WHERE `call_sign` = ? ORDER BY `date` ASC;";
+            $history_query = "SELECT `date`, `latitude`, `longitude`, `course`, `speed`, `altitude`, `daodatumbyte`, `comment` FROM `history` WHERE `call_sign` = ? " . ($since_date ? " AND `date` >= ? " : "") . " ORDER BY `date` ASC;";
             $history_stmt = $db->prepare($history_query);
-            $history_stmt->bind_param('s', $call_signs_row->call_sign);
+            if ($since_date)
+                $history_stmt->bind_param('ss', $call_signs_row->call_sign, $since_date);
+            else
+                $history_stmt->bind_param('s', $call_signs_row->call_sign);
             $history_stmt->execute();
             $history_result = $history_stmt->get_result();
             while ($packet = $history_result->fetch_object()) {
