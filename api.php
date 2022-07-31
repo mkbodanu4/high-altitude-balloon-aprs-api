959f7a43a74c4a429ec9ada1f0c5fd9a4fec9090
mkbodanu4 <mailmanbo@gmail.com>
2022-07-31 14:38:05 +0300

    Optimized map data loading

--- api.php
+++ api.php
@@ -26,57 +26,123 @@ switch ($get) {
                 $filter_to_date = date("Y-m-d H:i:s", $to_timestamp);
         }
 
+        $filter_south_west_lat = isset($_GET['south_west_lat']) ? trim(filter_var($_GET['south_west_lat'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION)) : NULL;
+        $filter_south_west_lng = isset($_GET['south_west_lng']) ? trim(filter_var($_GET['south_west_lng'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION)) : NULL;
+        $filter_north_east_lat = isset($_GET['north_east_lat']) ? trim(filter_var($_GET['north_east_lat'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION)) : NULL;
+        $filter_north_east_lng = isset($_GET['north_east_lng']) ? trim(filter_var($_GET['north_east_lng'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION)) : NULL;
+
         $filter_call_sign = isset($_GET['call_sign']) ? trim(filter_var($_GET['call_sign'], FILTER_SANITIZE_FULL_SPECIAL_CHARS)) : NULL;
 
         $balloons = array();
 
-        $query = "SELECT
+        $call_signs_where = array();
+        $call_signs_params = array();
+        if ($filter_call_sign) {
+            $call_signs_where[] = "`call_sign` = ?";
+            $call_signs_params[] = $filter_call_sign;
+        }
+        if ($filter_from_date && $filter_to_date) {
+            $call_signs_where[] = "(`date` >= ? AND `date` <= ?)";
+            $call_signs_params[] = $filter_from_date;
+            $call_signs_params[] = $filter_to_date;
+        } elseif ($filter_from_date && !$filter_to_date) {
+            $call_signs_where[] = "`date` >= ?";
+            $call_signs_params[] = $filter_from_date;
+        } elseif (!$filter_from_date && $filter_to_date) {
+            $call_signs_where[] = "`date` <= ?";
+            $call_signs_params[] = $filter_to_date;
+        }
+        if ($filter_south_west_lat !== NULL && $filter_south_west_lng !== NULL && $filter_north_east_lat !== NULL && $filter_north_east_lng !== NULL) {
+            $call_signs_where[] = "( `longitude` >= ? AND `longitude` <= ? AND `latitude` >= ? AND `latitude` <= ? )";
+            $call_signs_params[] = $filter_south_west_lng;
+            $call_signs_params[] = $filter_north_east_lng;
+            $call_signs_params[] = $filter_south_west_lat;
+            $call_signs_params[] = $filter_north_east_lat;
+        }
+        $call_signs_query = "SELECT
             `call_sign`
         FROM
             `history`
-         " . ($filter_call_sign ? " AND `call_sign` = ? " : "") . "
+        " . (count($call_signs_where) > 0 ? "WHERE " . implode(" AND ", $call_signs_where) . " " : "") . "
         GROUP BY
             `call_sign`;";
-        $call_signs_stmt = $db->prepare($query);
-        if ($filter_call_sign) {
-            $call_signs_stmt->bind_param('s', $filter_call_sign);
+        $call_signs_stmt = $db->prepare($call_signs_query);
+        if (count($call_signs_params) > 0) {
+            $call_signs_stmt->bind_param(str_repeat('s', count($call_signs_params)), ...$call_signs_params);
         }
         $call_signs_stmt->execute();
         $call_signs_result = $call_signs_stmt->get_result();
         while ($call_signs_row = $call_signs_result->fetch_object()) {
-            $balloon = array(
-                'call_sign' => $call_signs_row->call_sign,
-                'history' => array()
-            );
+            $balloon_history = array();
 
+            $history_where = array(
+                "`call_sign` = ?"
+            );
+            $history_params = array(
+                $call_signs_row->call_sign
+            );
+            if ($filter_from_date && $filter_to_date) {
+                $history_where[] = "(`date` >= ? AND `date` <= ?)";
+                $history_params[] = $filter_from_date;
+                $history_params[] = $filter_to_date;
+            } elseif ($filter_from_date && !$filter_to_date) {
+                $history_where[] = "`date` >= ?";
+                $history_params[] = $filter_from_date;
+            } elseif (!$filter_from_date && $filter_to_date) {
+                $history_where[] = "`date` <= ?";
+                $history_params[] = $filter_to_date;
+            }
+            if ($filter_south_west_lat !== NULL && $filter_south_west_lng !== NULL && $filter_north_east_lat !== NULL && $filter_north_east_lng !== NULL) {
+                $history_where[] = "( `longitude` >= ? AND `longitude` <= ? AND `latitude` >= ? AND `latitude` <= ? )";
+                $history_params[] = $filter_south_west_lng;
+                $history_params[] = $filter_north_east_lng;
+                $history_params[] = $filter_south_west_lat;
+                $history_params[] = $filter_north_east_lat;
+            }
             $history_query = "SELECT
-                `date`, `latitude`, `longitude`, `course`, `speed`, `altitude`, `daodatumbyte`, `comment`
+                `date`,
+                `latitude`,
+                `longitude`,
+                `course`,
+                `speed`,
+                `altitude`,
+                `comment`
             FROM
                 `history`
-            WHERE
-                `call_sign` = ?
-                " . ($filter_from_date && $filter_to_date ? " AND (`date` >= ? AND `date` <= ?) " : "") . "
-                " . ($filter_from_date && !$filter_to_date ? " AND `date` >= ? " : "") . "
-                " . (!$filter_from_date && $filter_to_date ? " AND `date` <= ? " : "") . "
+            " . (count($history_where) > 0 ? "WHERE " . implode(" AND ", $history_where) . " " : "") . "
             ORDER BY `date` ASC;";
             $history_stmt = $db->prepare($history_query);
-            if ($filter_from_date && $filter_to_date)
-                $history_stmt->bind_param('sss', $call_signs_row->call_sign, $filter_from_date, $filter_to_date);
-            elseif ($filter_from_date && !$filter_to_date)
-                $history_stmt->bind_param('ss', $call_signs_row->call_sign, $filter_from_date);
-            elseif (!$filter_from_date && $filter_to_date)
-                $history_stmt->bind_param('ss', $call_signs_row->call_sign, $filter_to_date);
-            else
-                $history_stmt->bind_param('s', $call_signs_row->call_sign);
+            if (count($history_params) > 0) {
+                $history_stmt->bind_param(str_repeat('s', count($history_params)), ...$history_params);
+            }
             $history_stmt->execute();
             $history_result = $history_stmt->get_result();
-            while ($packet = $history_result->fetch_object()) {
-                $balloon['history'][] = $packet;
+            $previous_latitude = NULL;
+            $previous_longitude = NULL;
+            while ($row = $history_result->fetch_object()) {
+                if ($previous_latitude !== NULL && $previous_longitude !== NULL && $previous_latitude === $row->latitude && $previous_longitude === $row->longitude) {
+                    // Discard previous element of array, if it has same location as current
+                    array_pop($balloon_history);
+                }
+                $packet = array(
+                    "d" => $row->date,
+                    "lat" => $row->latitude,
+                    "lng" => $row->longitude,
+                );
+                if ($row->course) $packet['crs'] = $row->course;
+                if ($row->speed) $packet['s'] = $row->speed;
+                if ($row->altitude) $packet['a'] = $row->altitude;
+                if ($row->comment) $packet['c'] = $row->comment;
+
+                $balloon_history[] = $packet;
+
+                $previous_latitude = $row->latitude;
+                $previous_longitude = $row->longitude;
             }
             $history_result->close();
 
 
-            $balloons[] = $balloon;
+            $balloons[$call_signs_row->call_sign] = $balloon_history;
         }
         $call_signs_result->close();
 
